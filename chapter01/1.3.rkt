24e5c28853ac71657dd4b88daac43f704f4c9661
0x6A6471 <hello@jakequinter.io>
2025-01-04 18:02:31 -0600

    1.3

---
+++ chapter01/1.3.rkt
@@ -0,0 +1,11 @@
+#lang racket
+
+(define (sum-of-squares x y z)
+  (define (square n) (* n n))
+  (cond ((and (<= x y) (<= x z)) (+ (square y) (square z)))
+        ((and (<= y x) (<= y z)) (+ (square x) (square z)))
+        (else (+ (square x) (square y)))))
+
+(sum-of-squares 1 2 3)
+(sum-of-squares 2 1 3)
+(sum-of-squares 3 2 1)
